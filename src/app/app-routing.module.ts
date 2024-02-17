1a36a6ecaa007e65d3c0748c571c4d1b20a26667
Tharusha Liyanage <henegamt@sheridancollege.ca>
2024-02-16 22:05:50 -0500

    commit 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+// app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstPageComponent },
+  { path: 'second-page/:cupcakeCount', component: SecondPageComponent },
+  { path: 'third-page/:cupcakeCount/:flavor', component: ThirdPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+
+const routes: Routes = [
+  { path: '', component: FirstPageComponent },
+  { path: 'second-page/:cupcakeCount', component: SecondPageComponent },
+  { path: 'third-page/:cupcakeCount/:flavor', component: ThirdPageComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent
   ],
   imports: [
     BrowserModule,
+    BrowserModule, RouterModule.forRoot(routes), FormsModule,
     AppRoutingModule
   ],
   providers: [],
